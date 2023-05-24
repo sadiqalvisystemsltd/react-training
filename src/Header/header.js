04e7d454515edc2af2a06a1690ea19366dbbd905
sadiq.alvi@visionet.com <sadiq.alvi@visionet.com>
2023-05-24 21:19:02 +0500

    bug resolved

--- src/Header/header.js
+++ src/Header/header.js
@@ -28,11 +28,17 @@ function Header(props) {
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
+              style={{ maxHeight: "100px"}}
               navbarScroll
             >
               <Nav.Link href="#home" onClick={onHomeClick}>Dashboard</Nav.Link>
-              {!props.loggedIn && <Nav.Link href="#login" onClick={onLoginClick}>Login</Nav.Link>}
+            </Nav>
+            <Nav
+              className="ms-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px"}}
+              navbarScroll
+            >
+              {!props.loggedIn && <Nav.Link href="#login" onClick={onLoginClick} >Login</Nav.Link>}
               {!props.loggedIn && <Nav.Link href="#signup" onClick={onSignupClick}>Signup</Nav.Link>}
               {props.loggedIn && <Nav.Link href="#logout" onClick={onLogout}>Logout</Nav.Link>}
             </Nav>
